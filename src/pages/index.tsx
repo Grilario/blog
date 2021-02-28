ed7c381b4e54ab2c20b7c63b6ffc8af567592eaa
Grilario <fazerbackup375@gmail.com>
2021-02-28 14:15:38 -0300

    google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import HomeNews from '../components/HomeNews'
 
+//Analytics
+import ReactGa from 'react-ga'
+ReactGa.initialize(process.env.ID_ANALYTICS)
+ReactGa.pageview(window.location.pathname + window.location.search)
+
 export default function Home() {
   return (
     <>
